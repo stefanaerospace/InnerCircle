de451e2459b138390fc38fdc741a90854bd046de
stefanaerospace <15001480+stefanaerospace@users.noreply.github.com>
2018-09-10 06:48:53 -0600

    First Commit--based on tutorial

---
+++ app/src/main/java/com/example/stefan/innercircle/Person.java
@@ -0,0 +1,13 @@
+package com.example.stefan.innercircle;
+
+public class Person {
+    String name;
+    String age;
+    int photoId;
+
+    Person(String name, String age, int photoId) {
+        this.name = name;
+        this.age = age;
+        this.photoId = photoId;
+    }
+}
---
+++ app/src/main/java/com/example/stefan/innercircle/RecyclerViewActivity.java
@@ -0,0 +1,45 @@
+package com.example.stefan.innercircle;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    private List<Person> persons;
+    private RecyclerView rv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.recyclerview);
+
+        rv = findViewById(R.id.rv);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+        initializeData();
+        initializeAdapter();
+    }
+
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.emma));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.lavery));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.lillie));
+
+    }
+
+    private void initializeAdapter(){
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);
+    }
+}
