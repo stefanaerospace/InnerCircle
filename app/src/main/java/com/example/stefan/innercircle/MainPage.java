e92fc142d284e8f97d37dd7b369aaedee6449820
stefanaerospace <15001480+stefanaerospace@users.noreply.github.com>
2019-06-17 19:51:01 -0600

    Update MainPage.java

--- app/src/main/java/com/example/stefan/innercircle/MainPage.java
+++ app/src/main/java/com/example/stefan/innercircle/MainPage.java
@@ -35,16 +35,11 @@ public class MainPage extends AppCompatActivity {
 
     private void initializeData(){
         persons = new ArrayList<>();
-        persons.add(new Person("Stefan Is the Shit", "Fuck yeah", R.drawable.emma));
-        persons.add(new Person("About time this POS worked", "-Everybody", R.drawable.lavery));
+        persons.add(new Person("Name1", "Sub1", R.drawable.emma));
+        persons.add(new Person("About time this worked", "-Everybody", R.drawable.lavery));
         persons.add(new Person("Now that it works...", "Time to have some fun!", R.drawable.lillie));
         persons.add(new Person("JK, it's Monday", "I have to go to work tomorrow", R.drawable.emma));
         persons.add(new Person("More updates tomorrow", "Or maybe Wednesday", R.drawable.lavery));
-        persons.add(new Person("More pretty faces", "God...", R.drawable.lillie));
-        persons.add(new Person("The guy that made this tutorial needs a gf", "I could probably do with one too", R.drawable.emma));
-        persons.add(new Person("lol jk", "I'm good.", R.drawable.lavery));
-        persons.add(new Person("Got shit to do", "Plus I am too cheap rn to have a gf", R.drawable.lillie));
-
     }
 
     private void initializeAdapter(){
